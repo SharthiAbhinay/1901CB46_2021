46d0fd437ac5a85a243c4bba6bb3c0f8f96f1ad7
SharthiAbhinay <sharthiabhinay@gmail.com>
2021-08-18 09:24:31 +0530

    Solution for tut_1

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,21 +1,28 @@
-n=int(input("give the number")) 
-ismerkai=True
-if n/10<1 and n<10:
-    print("yes")
-    quit()
-else:    
-    while n>=10:
-        j=int(n%10)
-        l=int(n/10)
-        i=int(l%10)
-        k=i-j
-        if k<0:
-            k=-k
-        if k!=1:
-            ismerkai=False
-            break
-        n=int(n/10)
-if ismerkai:
-    print("yes")
-else: 
-    print("no") 
+
+l=[12, 14, 56, 78, 98, 54, 678, 134, 789, 0, 7, 5, 123, 45, 76345,
+987654321]
+mer=0
+nmer=0
+for i in l:
+    n=i
+    ismerkai=True
+    if n/10<1 and n<10:
+        mer=mer+1
+        continue
+    else:    
+        while n>=10:
+            j=int(n%10)
+            l=int(n/10)
+            p=int(l%10)
+            k=p-j
+            if k<0:
+                k=-k
+            if k!=1:
+                ismerkai=False
+                break
+            n=int(n/10)
+    if ismerkai:
+        mer=mer+1
+    else: 
+        nmer=nmer+1 
+print("The input list contains",mer,"meraki values and",nmer,"non merkaivalues")
