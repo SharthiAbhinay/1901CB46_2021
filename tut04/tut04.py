a53c5dc6419f46eecd222d6e9344a7ddb74b5fab
SharthiAbhinay <sharthiabhinay@gmail.com>
2021-09-12 23:15:35 +0530

    tut04 solution

--- tut04/tut04.py
+++ tut04/tut04.py
@@ -2,53 +2,70 @@ import os
 import csv
 from openpyxl import Workbook
 from openpyxl import load_workbook
+
+folder1 = "output_by_subject"
  
+if not os.path.exists(folder1): 
+        os.makedirs(folder1)
+folder2 = "output_individual_roll"
+ 
+if not os.path.exists(folder2): 
+        os.makedirs(folder2)
+
 def output_by_subject():
-    directory = "output_by_subject"
- 
-    if not os.path.exists(directory): 
-        os.makedirs(directory)
+    head=['rollno','register_sem','subno','sub_type']
     with open(r'tut04\regtable_old.csv','r') as info:
         cinfo=csv.reader(info)
-        for data in cinfo:
-            del data[4:8]
-            del data[2:3]
-            if (data[2] =="subno"):continue
-            try:
-                with open(f"output_by_subject\\{data[2]}.xlsx"): 
-                    wb=Workbook()
-                    sheet1=wb.active
-                    sheet1.append(data)
-                    wb.save(f'output_by_subject\\{data[2]}.xlsx')
-            except IOError:
-                wb=load_workbook(r'output_by_subject\\{}.xlsx'.format(data[2]))
-                sheet1=wb.active
-                sheet1.append(data)
-                wb.save(f'output_by_subject\\{data[2]}.xlsx')
+        for words in cinfo:
+            given_data=[]
+            given_data.append(words[0])
+            given_data.append(words[1])
+            given_data.append(words[3])
+            given_data.append(words[-1])
+            if (given_data[2] =="subno"):continue
+            subno='{}.xlsx'.format(given_data[2])
+            path='./output_by_subject/'+subno
+        
+            if(os.path.isfile(path)): 
+                 book=load_workbook(r'output_by_subject\\{}.xlsx'.format(given_data[2]))
+                 sheet1=book.active
+                 sheet1.append(given_data)
+                 book.save(r'output_by_subject\\{}.xlsx'.format(given_data[2]))
+                 
+            else: 
+                    book=Workbook()
+                    sheet1=book.active
+                    sheet1.append(head)
+                    sheet1.append(given_data)
+                    book.save(f'output_by_subject\\{given_data[2]}.xlsx')
     return
  
 def output_individual_roll():
-    directory = "output_individual_roll"
- 
-    if not os.path.exists(directory): 
-        os.makedirs(directory)
+    head=['rollno','register_sem','subno','sub_type']
     with open(r'tut04\regtable_old.csv','r') as info:
         cinfo=csv.reader(info)
-        for data in cinfo:
-            del data[4:8]
-            del data[2:3]
-            if (data[0] =="rollno"):continue
-            try:
-                with open(f"output_individual_roll\\{data[0]}.xlsx"): 
-                    wb=Workbook()
-                    sheet2=wb.active
-                    sheet2.append(data)
-                    wb.save(f'output_individual_roll\\{data[0]}.xlsx')
-            except IOError:
-                wb=load_workbook(r'output_individual_roll\\{}.xlsx'.format(data[0]))
-                sheet2=wb.active
-                sheet2.append(data)
-                wb.save(f'output_individual_roll\\{data[0]}.xlsx')
+        for words in cinfo:
+            given_data=[]
+            given_data.append(words[0])
+            given_data.append(words[1])
+            given_data.append(words[3])
+            given_data.append(words[-1])
+            if (given_data[0] =="rollno"):continue
+            rollno='{}.xlsx'.format(given_data[0])
+            path='./output_individual_roll/'+rollno
+            if(os.path.isfile(path)): 
+                 book=load_workbook(r'output_individual_roll\\{}.xlsx'.format(given_data[0]))
+                 sheet2=book.active
+                 sheet2.append(given_data)
+                 book.save(r'output_individual_roll\\{}.xlsx'.format(given_data[0]))
+                 
+                    
+            else: 
+                    book=Workbook()
+                    sheet2=book.active
+                    sheet2.append(head)
+                    sheet2.append(given_data)
+                    book.save(f'output_individual_roll\\{given_data[0]}.xlsx')
     return
  
 output_by_subject()
