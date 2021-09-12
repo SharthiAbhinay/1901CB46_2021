f758d9fbb6b97f51aed67dd8986a6cc5ef2f1df2
SharthiAbhinay <sharthiabhinay@gmail.com>
2021-09-12 22:05:38 +0530

    tut04 solution

--- tut04/tut04.py
+++ tut04/tut04.py
@@ -1,6 +1,55 @@
+import os
+import csv
+from openpyxl import Workbook
+from openpyxl import load_workbook
+ 
 def output_by_subject():
+    directory = "output_by_subject"
+ 
+    if not os.path.exists(directory): 
+        os.makedirs(directory)
+    with open(r'tut04\regtable_old.csv','r') as info:
+        cinfo=csv.reader(info)
+        for data in cinfo:
+            del data[4:8]
+            del data[2:3]
+            if (data[2] =="subno"):continue
+            try:
+                with open(f"output_by_subject\\{data[2]}.xlsx"): 
+                    wb=Workbook()
+                    sheet=wb.active
+                    sheet.append(data)
+                    wb.save(f'output_by_subject\\{data[2]}.xlsx')
+            except IOError:
+                wb=load_workbook(r'output_by_subject\\{}.xlsx'.format(data[2]))
+                sheet=wb.active
+                sheet.append(data)
+                wb.save(f'output_by_subject\\{data[2]}.xlsx')
     return
-
+ 
 def output_individual_roll():
+    directory = "output_individual_roll"
+ 
+    if not os.path.exists(directory): 
+        os.makedirs(directory)
+    with open(r'tut04\regtable_old.csv','r') as info:
+        cinfo=csv.reader(info)
+        for data in cinfo:
+            del data[4:8]
+            del data[2:3]
+            if (data[0] =="rollno"):continue
+            try:
+                with open(f"output_individual_roll\\{data[0]}.xlsx"): 
+                    wb=Workbook()
+                    sheet=wb.active
+                    sheet.append(data)
+                    wb.save(f'output_individual_roll\\{data[0]}.xlsx')
+            except IOError:
+                wb=load_workbook(r'output_individual_roll\\{}.xlsx'.format(data[0]))
+                sheet=wb.active
+                sheet.append(data)
+                wb.save(f'output_individual_roll\\{data[0]}.xlsx')
     return
-
+ 
+output_by_subject()
+output_individual_roll()
