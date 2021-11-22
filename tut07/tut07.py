9f2fdaf508b5b8ea790e2b851f0d1d59d6044997
SharthiAbhinay <sharthiabhinay@gmail.com>
2021-11-23 00:05:03 +0530

    update

--- tut07/tut07.py
+++ tut07/tut07.py
@@ -4,13 +4,17 @@ import xlsxwriter
 from openpyxl import load_workbook
 import csv
 def feedback_not_submitted():
-    studentinfo_file=pd.read_csv(r'C:\Users\shart\OneDrive\Desktop\Python\1901CB46_2021\tut07\studentinfo.csv')
+    col_list1=["rollno","Name","email","aemail","contact"]
+    studentinfo_file=pd.read_csv(r'C:\Users\shart\OneDrive\Desktop\Python\1901CB46_2021\tut07\studentinfo.csv',usecols=col_list1)
     course_given=pd.read_csv(r'C:\Users\shart\OneDrive\Desktop\Python\1901CB46_2021\tut07\course_feedback_submitted_by_students.csv')
-    col_list=["rollno","subno"]
+    col_list2=["rollno","subno"]
     m_list=["subno","ltp"]
+    col_list3=["rollno","register_sem","schedule_sem","subno"]
+    course_h=pd.read_csv(r'C:\Users\shart\OneDrive\Desktop\Python\1901CB46_2021\tut07\course_registered_by_all_students.csv',usecols=col_list3)
     course_regi=pd.read_csv(r'C:\Users\shart\OneDrive\Desktop\Python\1901CB46_2021\tut07\course_registered_by_all_students.csv',usecols=col_list)
     master=pd.read_csv(r'C:\Users\shart\OneDrive\Desktop\Python\1901CB46_2021\tut07\course_master_dont_open_in_excel.csv',usecols=m_list)
     d=course_regi.applymap(str).groupby('rollno')['subno'].apply(list).to_dict()
+    df={}
     for key,value in d.items():
         for v in value:
             n=df[v]
@@ -18,7 +22,15 @@ def feedback_not_submitted():
                 continue
             seriesObj = course_given.apply(lambda x: True if x['stud_roll']==key and x['course_code']==v else False , axis=1)
             numOfRows = len(seriesObj[seriesObj == True].index)
-            if n!=numOfRows:
+            if n>snumOfRows:
+                c1=studentinfo_file.rollno==key
+                c2=course_h.rollno==key
+                c3=course_h.subno==v
+                c4=c2 & c3
+                h1=studentinfo_file[c1]
+                b=h1.drop("rollno",axis=1)
+                a=course_h[c4]
+                newdf=pd.concat([a,b],axis=1)
                 writer = pd.ExcelWriter(r'C:\Users\shart\OneDrive\Desktop\Python\1901CB46_2021\tut07\course_feedback_remaining.xlsx', engine='openpyxl')
                 writer.book = load_workbook(r'C:\Users\shart\OneDrive\Desktop\Python\1901CB46_2021\tut07\course_feedback_remaining.xlsx')
                 writer.sheets = dict((ws.title, ws) for ws in writer.book.worksheets)
