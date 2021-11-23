73e74832ba5e6583903f570d0aac3cc7ce89f4ee
SharthiAbhinay <sharthiabhinay@gmail.com>
2021-11-23 11:15:00 +0530

    solution

--- tut07/tut07.py
+++ tut07/tut07.py
@@ -4,17 +4,29 @@ import xlsxwriter
 from openpyxl import load_workbook
 import csv
 def feedback_not_submitted():
-    col_list1=["rollno","Name","email","aemail","contact"]
+    col_list1=["Roll No","Name","email","aemail","contact"]
     studentinfo_file=pd.read_csv(r'C:\Users\shart\OneDrive\Desktop\Python\1901CB46_2021\tut07\studentinfo.csv',usecols=col_list1)
     course_given=pd.read_csv(r'C:\Users\shart\OneDrive\Desktop\Python\1901CB46_2021\tut07\course_feedback_submitted_by_students.csv')
     col_list2=["rollno","subno"]
     m_list=["subno","ltp"]
     col_list3=["rollno","register_sem","schedule_sem","subno"]
     course_h=pd.read_csv(r'C:\Users\shart\OneDrive\Desktop\Python\1901CB46_2021\tut07\course_registered_by_all_students.csv',usecols=col_list3)
-    course_regi=pd.read_csv(r'C:\Users\shart\OneDrive\Desktop\Python\1901CB46_2021\tut07\course_registered_by_all_students.csv',usecols=col_list)
+    course_regi=pd.read_csv(r'C:\Users\shart\OneDrive\Desktop\Python\1901CB46_2021\tut07\course_registered_by_all_students.csv',usecols=col_list2)
     master=pd.read_csv(r'C:\Users\shart\OneDrive\Desktop\Python\1901CB46_2021\tut07\course_master_dont_open_in_excel.csv',usecols=m_list)
     d=course_regi.applymap(str).groupby('rollno')['subno'].apply(list).to_dict()
-    df={}
+    c=master['ltp'].str.split('-')
+    sf = c.tolist()
+    h=[]
+    ans=master['subno']
+    for i in sf:
+     count=0
+     for j in i:
+        if j>'0' :
+            count=count+1
+    h.append(count)
+    l=pd.DataFrame(h,columns=['nonzero'])
+    h=pd.concat([ans,l],axis=1)
+    df=h.set_index('subno').to_dict()['nonzero']
     for key,value in d.items():
         for v in value:
             n=df[v]
@@ -22,7 +34,7 @@ def feedback_not_submitted():
                 continue
             seriesObj = course_given.apply(lambda x: True if x['stud_roll']==key and x['course_code']==v else False , axis=1)
             numOfRows = len(seriesObj[seriesObj == True].index)
-            if n>snumOfRows:
+            if n>numOfRows:
                 c1=studentinfo_file.rollno==key
                 c2=course_h.rollno==key
                 c3=course_h.subno==v
@@ -31,18 +43,20 @@ def feedback_not_submitted():
                 b=h1.drop("rollno",axis=1)
                 a=course_h[c4]
                 newdf=pd.concat([a,b],axis=1)
-                writer = pd.ExcelWriter(r'C:\Users\shart\OneDrive\Desktop\Python\1901CB46_2021\tut07\course_feedback_remaining.xlsx', engine='openpyxl')
-                writer.book = load_workbook(r'C:\Users\shart\OneDrive\Desktop\Python\1901CB46_2021\tut07\course_feedback_remaining.xlsx')
-                writer.sheets = dict((ws.title, ws) for ws in writer.book.worksheets)
-                newdf.to_excel(writer,index=False,header=False,startrow=len(reader)+1)
+                path=r'C:\Users\shart\OneDrive\Desktop\Python\1901CB46_2021\tut07\course_feedback_remaining.xlsx'
+                workbook = openpyxl.load_workbook(path)
+                writer = pd.ExcelWriter(path, engine='openpyxl')
+                writer.book = workbook
+                writer.sheets = dict((ws.title, ws) for ws in workbook.worksheets)
+                data_df.to_excel(writer, 'course_feedback_remaining.xlsx')
+                writer.save()
                 writer.close()
 
 
 
 
 
-    ltp_mapping_feedback_type = {1: 'lecture', 2: 'tutorial', 3:'practical'}
-    output_file_name = "course_feedback_remaining.xlsx" 
+     
  
 
 
